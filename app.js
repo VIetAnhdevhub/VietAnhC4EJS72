a02373ba4fa59d4bcc76bfde23a28c0af7cfd8e9
VũĐìnhViệtAnh <vudinhvietanh1@MacBook-Pro-cua-Luu.local>
2020-02-14 17:43:49 +0700

    demo

---
+++ app.js
@@ -0,0 +1,16 @@
+6;
+let x = Number(prompt("Enter a number"));
+if (x < 4.5) {
+  alert("Lower half of 9");
+} else {
+  alert("Higher half of 9");
+}
+
+// 7
+// let n = Number(prompt("Enter a number"));
+// let x = Number(prompt("Enter a number"));
+// if (x < n / 2) {
+//   alert(`${x} in lower half of ${n}`);
+// } else if (x > n / 2) {
+//   alert(`${x} in higher half of ${n}`);
+// }
